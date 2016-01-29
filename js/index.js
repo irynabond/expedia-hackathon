ca68311a158beeab63841327ec8f9ce24259aae0
irusiabondarenko <irusiabondarenko@gmail.com>
2016-01-28 20:30:23 -0800

    Access to tickets' price established

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 
-var Requester = require('requester'),
-requester = new Requester({debug: 1});
+var request = require('request');
 
 module.exports = function expedia_api (){
-	requester.get("http://terminal2.expedia.com:80/x/mflights/search?departureDate=2016-02-18&departureAirport=SEA&arrivalAirport=ABR&prettyPrint=true&apikey=TkefBxxLZbMGgOvKRGFLIkYJxcB3bYtA", function (body) {
-    console.log (this.statusCode, body);
+	request("http://terminal2.expedia.com:80/x/mflights/search?departureDate=2016-02-18&departureAirport=SEA&arrivalAirport=ABR&prettyPrint=true&&maxOfferCount=2&apikey=TkefBxxLZbMGgOvKRGFLIkYJxcB3bYtA", function (error, res, body) {
+    var obj = JSON.parse(res.body);
+    var results = obj.offers;
+    for (var i = 0; i<results.length; i++) {
+    	console.log (results[i].baseFarePrice.formattedWholePrice);
+    }
  	});
 };
